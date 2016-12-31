3b0d9bae2314f7899d02e99611b085553445d115
Marc Rohloff <marc@marcrohloff.com>
2016-12-31 13:55:13 -0700

    Fix ResponseError message and add method to return message without code

--- lib/weeblycloud/cloudclient/exceptions.rb
+++ lib/weeblycloud/cloudclient/exceptions.rb
@@ -7,9 +7,16 @@ module Weeblycloud
     def initialize(msg = "Unknown error occured", code)
       @code = code
       @message = msg
-      m = "(CODE: \##{@code}) #{@msg}"
+      m = "(CODE: \##{@code}) #{@message}"
       super(m)
     end
+
+    def message_without_code
+      matches = /(.*?)(\s+\(\d+\))?$/.match(message)
+      matches && matches[1]
+    end
+    alias display_message message_without_code
+
   end
 
   # Raised with invalid pagination method invocation
