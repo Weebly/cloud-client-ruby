39f0497c27b9129abc9a571bde99ab86fb739665
Marc Rohloff <marc@marcrohloff.com>
2017-01-02 13:43:23 -0700

    Fix CloudResource array setter

--- lib/weeblycloud/saveable.rb
+++ lib/weeblycloud/saveable.rb
@@ -13,21 +13,22 @@ module Weeblycloud
         if @got
           return nil
         else
-          @got = true
           get()
+          @got = true
           return set_property(prop, value)
         end
       end
     end
 
     # Set a property using the [] setter
-    def []=(prop)
-      set_property(prop)
+    def []=(prop, value)
+      set_property(prop, value)
     end
 
     # Make an API call to save changes to the resource
     def save
       @client.patch(@endpoint, :content=>@changed)
+      @changed = {}
       return nil
     end
 
